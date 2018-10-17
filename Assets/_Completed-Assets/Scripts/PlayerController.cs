b8ec94f3f6c8fc7485359d02b7b238e66ba105d8
Matthew Kelly <mkonabike@gmail.com>
2018-10-17 15:50:58 -0500

    added collision dection between player object and pickups, pickups disappearing from screen

--- Assets/_Completed-Assets/Scripts/PlayerController.cs
+++ Assets/_Completed-Assets/Scripts/PlayerController.cs
@@ -32,4 +32,14 @@ public class PlayerController : MonoBehaviour {
 		// USE ADDForce FUNCTION TO CREATE 2D PHYSICS ON RigidBody2D COMPONENT 
 		rb2d.AddForce (movement * speed);
 	}
+
+	//OnTriggerEnter2D is called whenever this object overlaps with a trigger collider.
+	void OnTriggerEnter2D(Collider2D other) 
+	{
+		//Check the provided Collider2D parameter other to see if it is tagged "PickUp", if it is...
+		if (other.gameObject.CompareTag("Pickup"))
+		{
+			other.gameObject.SetActive(false);
+		}
+	}
 }
